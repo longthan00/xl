a0240078a08b8c8ef5179817e3da9740062f6793
longthan00 <duongthach209l@gmail.com>
2025-11-07 20:29:51 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+
+yesBtn.addEventListener("click", () => {
+  document.body.innerHTML = `
+    <div style="text-align:center; margin-top:100px;">
+      <h1>Yeee cảm ơn Duyên nhìuuuuu 😍💞</h1>
+      <p>Mai thạch đưa milo nhéee 💖</p>
+      <img src="https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif" width="250"/>
+    </div>
+  `;
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - noBtn.clientWidth);
+  const y = Math.random() * (window.innerHeight - noBtn.clientHeight);
+  noBtn.style.position = "absolute";
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+});
